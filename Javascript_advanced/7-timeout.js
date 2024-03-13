61bb576e42883d4005082aa943c01f8667f99b35
Frandy Slueue <frandy4ever@gmail.com>
2024-03-13 10:22:51 -0500

    use setTimeout function

---
+++ Javascript_advanced/7-timeout.js
@@ -0,0 +1,13 @@
+console.log('Start of the execution queue');
+
+setTimeout(() => {
+  console.log('Final code block to be executed');
+}, 0);
+
+let i = 1;
+while (i <= 100) {
+  console.log(i);
+  i++;
+}
+
+console.log('End of the loop printing');
