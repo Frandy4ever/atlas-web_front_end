8d4e5c49937d59441bf2f6351e630a00e7e157b9
Frandy Slueue <frandy4ever@gmail.com>
2024-03-13 09:43:21 -0500

    work with complex js closures

---
+++ Javascript_advanced/4-math.js
@@ -0,0 +1,17 @@
+const divideBy(firstNumber) {
+  return function(secondNumber) {
+    return secondNumber / firstNumber;
+  }
+}
+
+const addBy = (firstNumber) => {
+  return function(secondNumber) {
+    return firstNumber + secondNumber;
+  }
+}
+
+// Closures
+const addBy100 = addBy(100);
+const addBy1000 = addBy(1000);
+const divideBy10 = divideBy(10);
+const divideBy100 = divideBy(100);
